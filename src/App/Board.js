9ce7104db9f4dc9d25879f3c77f95b7238e5d328
Nae Bara <natanaelbarag@gmail.com>
2020-12-10 16:25:12 +0200

    v1

--- src/App/Board.js
+++ src/App/Board.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Square from './Square'
 
 export default class Board extends Component {
@@ -7,27 +7,62 @@ export default class Board extends Component {
 
 		this.state = {
 			squares: Array(9).fill(null),
-			isXTurn: true
+			isXTurn: true,
+			winner: '',
+			over: false
 		}
 	}
 
+	calculateWinner(squares) {
+		const lines = [
+			[0, 1, 2],
+			[3, 4, 5],
+			[6, 7, 8],
+			[0, 3, 6],
+			[1, 4, 7],
+			[2, 5, 8],
+			[0, 4, 8],
+			[2, 4, 6],
+		];
+		for (let i = 0; i < lines.length; i++) {
+			const [a, b, c] = lines[i];
+			if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+				console.log("won")
+				return squares[a];
+			}
+		}
+		return null;
+	}
+
 	onSquareClick(index) {
+
 		const squares = [...this.state.squares];
-		squares[index] = this.state.isXTurn ? 'X' : '0'
-		this.setState({
-			squares,
-			isXTurn: !this.state.isXTurn
-		})
+		if (squares[index] == null && this.state.over == false) {
+			squares[index] = this.state.isXTurn ? 'X' : '0'
+			this.setState({
+				squares,
+				isXTurn: !this.state.isXTurn
+			})
+
+			if (this.calculateWinner(squares) != null) {
+
+				this.state.over = true;
+				this.state.winner = this.calculateWinner(squares);
+				setTimeout(() => alert("Winner is " + this.state.winner), 200);
+
+			}
+		}
+
 	}
 
 	render() {
 		return <div className="board">
 			{
-				this.state.squares.map( 
+				this.state.squares.map(
 					(value, index) => <Square key={index}
-					value={value}
-					onSquareClick={() => this.onSquareClick(index)}
-				/>)
+						value={value}
+						onSquareClick={() => this.onSquareClick(index)}
+					/>)
 			}
 		</div>
 	}
